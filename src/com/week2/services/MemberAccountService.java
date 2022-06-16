73d808aa065b0bf38b2c6750b3363d9b31d6fe61
mustafaozdemiir <mustafaaozdemiir@gmail.com>
2022-06-16 19:10:10 +0300

    Comment lines added.

--- src/com/week2/services/MemberAccountService.java
+++ src/com/week2/services/MemberAccountService.java
@@ -2,16 +2,16 @@ package com.week2.services;
 
 public class MemberAccountService {
 // MemberAccount için crud işlem metodları oluşturuldu.
-    public static void create(){
+    public static void create(MemmberAccount member){
         System.out.println("Üye hesabı oluşturuldu.");
     }
     public static void read(){
         System.out.println("Üye bilgileri getirildi.");
     }
-    public static void update(){
+    public static void update(MemmberAccount member,MemmberAccount newMember){
         System.out.println("Üye bilgileri güncellendi.");
     }
-    public static void delete(){
+    public static void delete(MemberAccount member){
         System.out.println("Üye silindi.");
     }
 }
